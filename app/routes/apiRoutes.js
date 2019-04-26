cb4dc3a1b7fc238fa744cc376ec660dcda2fd077
Louie Fitzpatrick <fitz4492@gmail.com>
2019-04-25 22:59:20 -0500

    the final commit

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// We are linking our route to a "data" source.
+// The data source holds the array of information on friends
 // ===============================================================================
 
 var friends = require("../data/friends");
@@ -13,8 +13,6 @@ var friends = require("../data/friends");
 module.exports = function(app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
